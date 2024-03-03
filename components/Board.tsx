e8ca144ebe8a34f41c218171539aecfbb597117f
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-03 23:57:20 +0500

    Appwrite added and Header with gradient added

---
+++ components/Board.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const Board = () => {
+  return (
+    <>Hell</>
+    // <DragDropContext onDragEnd={}>
+    //     <Droppable droppableId="board" direction="horizontal" type="column">
+
+    //     </Droppable>
+    // </DragDropContext>
+  )
+}
+
+export default Board
